829c9375bc88f3e331323034d07b82691ff870ac
Adam <adam@iii.com>
2018-11-19 20:26:01 +0800

    test visual studio git

---
+++ HelloCSharpGit/Code2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloApp2
+{
+    class Code2
+    {
+        public void test()
+        {
+            Console.WriteLine("Test Different Namespace");
+        }
+    }
+}
+
+namespace HelloApp
+{
+    class Code3
+    {
+        public int test()
+        {
+            return 555;
+        }
+    }
+}
---
+++ HelloCSharpGit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class HelloWorld
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hi, this is my first C Sharp code.");
+            HelloApp.Hello2 hi = new HelloApp.Hello2();
+            hi.test();
+
+            HelloApp2.Code2 hi2 = new HelloApp2.Code2();
+            hi2.test();
+
+            Hello2 h3 = new Hello2();
+            Console.WriteLine(h3.test2());
+
+            Code3 h4 = new Code3();
+            Console.WriteLine(h4.test());
+        }
+    }
+
+    class Hello2
+    {
+        public void test()
+        {
+            Console.WriteLine("Test Method");
+        }
+
+        public String test2()
+        {
+            return "Kimi";
+        }
+    }
+
+}
